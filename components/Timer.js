3d7e47c80dde0572bc0aa3cbea8a73d69381c883
Kaan Pamukcu <alpikaan.pamukcu@gmail.com>
2025-01-03 13:26:17 -0500

    timer component added

--- components/Timer.js
+++ components/Timer.js
@@ -1,1 +1,29 @@
 //tracks the time left for the player to answer
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Timer = ({ duration, onTimeUp }) => {
+    const [timeLeft, setTimeLeft] = useState(duration);
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            onTimeUp();
+            return;
+        }
+        const timerId = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+        return () => clearInterval(timeId);
+    }, [timeLeft]);
+
+    return <Text style={StyleSheet.timerText}>{timeLeft}s</Text>;
+};
+
+const styles = StyleSheet.create({
+    timerText: {
+        fontSize: 20,
+        color: '#ff0000',
+    },
+});
+
+export default Timer;
